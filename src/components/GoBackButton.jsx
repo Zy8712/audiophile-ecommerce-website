98321dfbf3986ab45b909ff81064b04be8ff9244
Bryan Li <76718507+Zy8712@users.noreply.github.com>
2023-12-09 03:48:26 +0000

    minor commit, updated Go Back button
    Specific:
    - added arrow for go back button
    - added hover effects for go back button (needs debugging)
    - MISSING FROM PREVIOUS COMMIT MESSAGE: updated cart functionality so that when cart is opened and user clicks on any area of the screen which has the darkened effect applied, the cart will toggle close

--- src/components/GoBackButton.jsx
+++ src/components/GoBackButton.jsx
@@ -1,14 +1,22 @@
 import { Link } from 'wouter';
 
+import LeftArrow from "../assets/shared/desktop/arrow-circle-left-svgrepo-com.svg";
+import "../App.css"
+
 function GoBackButton(props) {
 
     return (
         <>
             <div className="block text-[15px] font-medium leading-[25px] opacity-50 mb-14">
                 <Link to={props.backLink}>
-                    <span className="text-black hover:text-theme-dark-orange cursor-pointer">
-                        Go Back
-                    </span>
+                    <div className="flex items-center group">
+                        <div className="inline group custom-dark-orange-filter-v2">
+                            <img src={LeftArrow} className="h-5 mr-2 transition duration-300 ease-in-out transform group-hover:-translate-x-1" />
+                        </div>
+                        <span className="text-black font-bold transition duration-300 ease-in-out cursor-pointer group-hover:text-theme-dark-orange">
+                            Go Back
+                        </span>
+                    </div>
                 </Link>
             </div>
         </>
